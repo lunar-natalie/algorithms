8463b86f342427813cc49595679a838a93e2ec8b
Natalie Wiggins <islifepeachy@outlook.com>
2024-01-28 02:09:00 +0000

    Use bounds checking in insertion sort

--- lib/Algorithms/Sort/InsertionSort.hpp
+++ lib/Algorithms/Sort/InsertionSort.hpp
@@ -11,15 +11,15 @@ void insertionSort(std::array<T, N> & arr)
 {
     // Assume the first value is sorted
     for (auto i = 1; i < N; ++i) {
-        auto key = arr[i];
+        auto key = arr.at(i);
         auto end = i - 1;
-        while (end >= 0 && arr[end] > key) {
+        while (end >= 0 && arr.at(end) > key) {
             // Shift values left
-            arr[end + 1] = arr[end];
+            arr.at(end + 1) = arr[end];
             --end;
         }
         // Restore key at the end of the range
-        arr[end + 1] = key;
+        arr.at(end + 1) = key;
     }
 }
 }// namespace Algorithms
